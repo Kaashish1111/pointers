87306b000d2a60cdd2828baffe64ed06e6c0c659
Kashish Goyal <kashish1782004@gmail.com>
2024-05-06 17:54:29 +0530

    new_file

---
+++ array_function_pointer.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+void add(int a,int b){
+    printf("sum is %d",a+b);
+}
+void sub(int a,int b){
+    printf("sub is %d",a-b);
+}
+void mul(int a,int b){
+    printf("mul is %d",a*b);
+}
+void div(int a,int b){
+    printf("div is %d",a/b);
+}
+int main(){
+    int value;
+    printf("Enter a value (0,1,2,3):");
+    scanf("%d",&value);
+    void (*ftr[10])(int,int)={add,sub,mul,div};
+    int a,b;
+    printf("Enter two number on which you wnat to perfom the operation: ");
+    scanf("%d%d",&a,&b);
+    (*ftr[value])(a,b);
+    return 0;
+}
